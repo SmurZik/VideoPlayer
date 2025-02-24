5fd645eaf3cf767c099a17a9614ae44804c12b6b
smurz <murca2403@gmail.com>
2025-02-24 20:19:48 +0300

    public api key

--- app/src/main/java/com/smurzik/videoplayer/list/data/cloud/VideoService.kt
+++ app/src/main/java/com/smurzik/videoplayer/list/data/cloud/VideoService.kt
@@ -1,13 +1,14 @@
 package com.smurzik.videoplayer.list.data.cloud
 
-import com.smurzik.videoplayer.BuildConfig
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface VideoService {
 
-    @Headers("Authorization: ${BuildConfig.apiKey}")
+    @Headers("Authorization: $API_KEY")
     @GET("search?query=nature&size=small&")
     suspend fun getVideos(@Query("page") page: Int): VideoItemCloud
 }
+
+const val API_KEY = "hOKqUk1gOjkg27DSgoZ3hpxUCrwhAC6XZfo8aYSFMd3Qj8ecpFfF22k1"
